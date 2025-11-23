c89aca121ed4f19bbfd8f6709113b2e7478a961b
Amin Mozaffarin <a.mozaffarin@mozys.de>
2025-11-23 17:32:35 +0100

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  ssr: false,
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
   ui: {
     colorMode: false,
   },
-});
+})
